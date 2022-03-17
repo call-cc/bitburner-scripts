a70d9a31c5be4447623770ad873e41397d08b3d8
Peter Barabas <peter.barabas@logmein.com>
2022-03-18 00:46:05 +0100

    Print to terminal

--- ns2/total-ram.js
+++ ns2/total-ram.js
@@ -2,13 +2,11 @@ import { scan } from './ns2/scan.js';
 
 /** @param {import("/.").NS} ns */
 export async function main(ns) {
-    ns.clearLog();
-    ns.tail();
     let totalRam = 0;
     for (let server of [...scan(ns)]) {
         const so = ns.getServer(server);
         totalRam += so.maxRam;
     }
     const str = ns.nFormat(totalRam * 1e9, '0.0a');
-    ns.print(`Total RAM: ${str}`);
+    ns.tprint(`Total RAM: ${str}`);
 }
